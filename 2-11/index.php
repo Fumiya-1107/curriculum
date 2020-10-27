596e82466037382692c04a4948f9c8b7921593f3
Fumiya-1107 <f.tarumac@gmail.com>
2020-10-28 00:03:02 +0900

    課題提出 2-11
    フォームに入力した内容を取得する・用語

---
+++ 2-11/index.php
@@ -0,0 +1,10 @@
+<form action="result.php" method="post">
+	氏名<input type="text" name="full_name">
+	<br>
+	パスワード<input type="password" name="password">
+	<br>
+	メールアドレス<input type="text" name="mail_address">
+	<br>
+<input type="submit" value="送　信">
+<input type="reset" value="クリア">
+</form>
---
+++ 2-11/result.php
@@ -0,0 +1,11 @@
+<?php
+
+$full_name = $post['full_name'];
+$password = $post['password'];
+$mail_address = $post['mail_address'];
+
+?>
+
+<p>氏名：<?php echo $full_name; ?></p>
+<p>パスワード：<?php echo $password; ?></p>
+<p>メールアドレス：<?php echo $mail_address; ?></p>
