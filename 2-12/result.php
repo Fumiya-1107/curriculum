0f0cb98a38dc8044e776f9f052a0a0ba9016386f
Fumiya-1107 <f.tarumac@gmail.com>
2020-10-29 20:36:00 +0900

    課題提出 (2-12)
    いろいろなフォームの入力欄・用語

---
+++ 2-12/result.php
@@ -0,0 +1,8 @@
+<?php
+	$name = $post['name'];
+	$product = $post['product'];
+	$number = $post['number'];
+?>
+	<p>お名前：<?php echo $name; ?></p>
+	<p>ご希望商品<?php echo $product; ?></p>
+	<p>個数：<?php echo $number; ?></p>
