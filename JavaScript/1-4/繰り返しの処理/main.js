1c33c73aae70f2d5bac9633d7b2c89dc9443d1be
Fumiya-1107 <f.tarumac@gmail.com>
2021-07-20 22:13:02 +0900

    JS1-4　繰り返し処理
    課題再提出

--- JavaScript/1-4/繰り返しの処理/main.js
+++ JavaScript/1-4/繰り返しの処理/main.js
@@ -1,17 +1,13 @@
-let number = 1;
+// let number = 1;
 
-while (number <= 100) {
-	console.log(number);
-	number++;
-
-	if (number % 3 == 0 && number % 5 == 0) {
-		console.log("FizzBuzz");
-		number++;
-	} else if (number % 3 == 0) {
-		console.log("Fizz");
-		number++;
-	} else if (number % 5 == 0) {
-		console.log("Buzz");
-		number++;
+for (let number=1; number<=100; number++) {
+	if (number%3 == 0 && number%5 !== 0) {
+		console.log("Fizz!");
+	} else if (number%5 == 0 && number%3 !== 0) {
+		console.log("Buzz!");
+	} else if (number%3 == 0 && number%5 == 0) {
+		console.log("FizzBuzz!!");
+	} else {
+		console.log(number);
 	}
 }
